52d121a8b24d35a3a26bfb134b11f930c126f6ab
diduladishan <diduladishan14@gmail.com>
2025-01-19 12:59:46 +0530

    feat: configure react leaflet in Map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,10 @@
 import { Navigate, useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import styles from "./Map.module.css";
 
 export default function Map() {
   const navigate = useNavigate();
+
   const [searchParams, setSearchParams] = useSearchParams();
 
   const lat = searchParams.get("lat");
@@ -10,9 +12,17 @@ export default function Map() {
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>
-        Position: {lat} , {lng}
-      </h1>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
